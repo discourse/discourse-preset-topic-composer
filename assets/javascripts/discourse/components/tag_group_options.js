f5ddacc1195ff41683e92197b08a926e2a0c3942
Gabriel Grubba <gabriel@discourse.org>
2024-05-16 23:24:26 -0300

    DEV: Add i18n for composer error message.

--- assets/javascripts/discourse/components/tag_group_options.js
+++ assets/javascripts/discourse/components/tag_group_options.js
@@ -1,6 +1,7 @@
 import { tracked } from "@glimmer/tracking";
 import { inject as service } from "@ember/service";
 import { ajax } from "discourse/lib/ajax";
+import I18n from "I18n";
 
 export const tagGroupOptions = {
   classNames: ["tag-group-input"],
@@ -20,29 +21,36 @@ export const tagGroupOptions = {
 
   init() {
     this._super(...arguments);
-    ajax(
-      `topic_composer/tag_by_tag_group/${this.tagGroupName}.json`
-    ).then((result) => (this.content = result.tags));
-    for (const option of this.selectKitOptions) {
-      if ("translatedNone" in option) {
-        option.translatedNone = this.tagGroupName;
-      }
-    }
+    ajax(`topic_composer/tag_by_tag_group/${this.tagGroupName}.json`).then(
+      (result) => (this.content = result.tags)
+    );
+    this.refreshText();
     this.composer.tag_groups[this.tagGroupName] = {
       value: [],
-      component: this
+      component: this,
     };
   },
 
-
   invalidate() {
     this.isInvalid = true;
+    this.refreshText();
   },
-
+  refreshText() {
+    for (const option of this.selectKitOptions) {
+      if ("translatedNone" in option) {
+        option.translatedNone = this.isInvalid
+          ? I18n.t("composer.error_message", {
+              tag_group_name: this.tagGroupName,
+            })
+          : this.tagGroupName;
+      }
+    }
+  },
   actions: {
     onChange(tagId) {
       this.value = tagId;
       this.isInvalid = false;
+      this.refreshText();
 
       const getTagById = (id) => this.content.find((tag) => tag.id === id);
       if (typeof tagId === "number") {
