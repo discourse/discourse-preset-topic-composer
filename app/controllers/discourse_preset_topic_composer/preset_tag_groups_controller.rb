bb80f342f063ae7ccfbb9faa1cc5e58c9b14cf02
Osman Gormus <osman@unity3d.com>
2024-07-02 16:28:08 +0200

    respect tags_sort_alphabetically setting

--- app/controllers/discourse_preset_topic_composer/preset_tag_groups_controller.rb
+++ app/controllers/discourse_preset_topic_composer/preset_tag_groups_controller.rb
@@ -7,7 +7,14 @@ module ::DiscoursePresetTopicComposer
     def search_tags_by_tag_group
       tag_group = params[:tag_group]
       tag_group = CGI.unescape(tag_group)
-      tags = TagGroup.visible(guardian).find_by(name: tag_group)&.tags || []
+
+      if SiteSetting.tags_sort_alphabetically
+        tag_order = "name ASC"
+      else
+        tag_order = "public_topic_count DESC"
+      end
+
+      tags = TagGroup.visible(guardian).find_by(name: tag_group)&.tags.order(tag_order) || []
       render json: { tags: tags }
     end
   end
