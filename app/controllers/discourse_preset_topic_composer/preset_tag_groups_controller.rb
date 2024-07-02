7bed6ca73a6d9a3a5157bbc729336bd6074afcba
Osman Gormus <osman@gorm.us>
2024-07-02 18:04:54 +0200

    Update app/controllers/discourse_preset_topic_composer/preset_tag_groups_controller.rb
    Co-authored-by: Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>

--- app/controllers/discourse_preset_topic_composer/preset_tag_groups_controller.rb
+++ app/controllers/discourse_preset_topic_composer/preset_tag_groups_controller.rb
@@ -8,11 +8,12 @@ module ::DiscoursePresetTopicComposer
       tag_group = params[:tag_group]
       tag_group = CGI.unescape(tag_group)
 
-      if SiteSetting.tags_sort_alphabetically
-        tag_order = "name ASC"
-      else
-        tag_order = "public_topic_count DESC"
-      end
+      tag_order =
+        if SiteSetting.tags_sort_alphabetically
+          "name ASC"
+        else
+          "public_topic_count DESC"
+        end
 
       tags = TagGroup.visible(guardian).find_by(name: tag_group)&.tags&.order(tag_order) || []
       render json: {
