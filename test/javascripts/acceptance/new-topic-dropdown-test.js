555ca119d275ce33550b5366127d9ac594f037b5
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-09-13 17:21:41 +0200

    DEV: trigger app event on topic dropdown change

---
+++ test/javascripts/acceptance/new-topic-dropdown-test.js
@@ -0,0 +1,42 @@
+import { getOwner } from "@ember/owner";
+import { visit } from "@ember/test-helpers";
+import { test } from "qunit";
+import { acceptance, resetSite } from "discourse/tests/helpers/qunit-helpers";
+import selectKit from "discourse/tests/helpers/select-kit-helper";
+
+acceptance(
+  "DiscoursePresetTopicComposer - new topic dropdown",
+  function (needs) {
+    needs.user();
+
+    needs.hooks.beforeEach(() => {
+      resetSite({
+        topic_preset_buttons: [
+          {
+            id: "NEW_BUG",
+            name: "Report a bug",
+          },
+        ],
+      });
+    });
+
+    test("app event on change", async function (assert) {
+      let triggered = false;
+      const appEvents = getOwner(this).lookup("service:app-events");
+      appEvents.on(
+        "discourse-preset-topic-composer:new-topic-preset-selected",
+        () => {
+          triggered = true;
+        }
+      );
+
+      await visit("/");
+
+      const newTopicDropdown = selectKit(".new-topic-dropdown");
+      await newTopicDropdown.expand();
+      await newTopicDropdown.selectRowByValue("NEW_BUG");
+
+      assert.ok(triggered);
+    });
+  }
+);
