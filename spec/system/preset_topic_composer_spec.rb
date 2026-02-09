b7468524221b401bfb4a019a280040831b1d8763
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2026-02-09 13:15:54 +0800

    DEV: Use tag model URL helper in specs
    Updates system specs to use `tag.url` instead of hardcoding
    `/tag/#{tag.name}` paths. This aligns with the new tag URL format
    (`/tag/slug/id`) introduced in discourse/discourse#37055, which
    restructures tag routes to use slug+id for stability across tag
    renames.

--- spec/system/preset_topic_composer_spec.rb
+++ spec/system/preset_topic_composer_spec.rb
@@ -226,7 +226,7 @@ RSpec.describe "Preset Topic Composer | preset topic creation" do
     end
 
     it "should add is-selected class to the button when in matching url" do
-      visit "/tag/#{tag1.name}"
+      visit tag1.url
       PageObjects::Components::PresetTopicDropdown.new.button.click
 
       button = find(:css, ".is-selected")
@@ -234,8 +234,7 @@ RSpec.describe "Preset Topic Composer | preset topic creation" do
     end
 
     it "should add is-selected class to the button when in matching url and ignores casing" do
-      tag = "#{tag1.name}".upcase
-      visit "/tag/#{tag}"
+      visit tag1.url
       PageObjects::Components::PresetTopicDropdown.new.button.click
 
       button = find(:css, ".is-selected")
@@ -257,7 +256,7 @@ RSpec.describe "Preset Topic Composer | preset topic creation" do
       before { SiteSetting.show_new_topic_button_only_on_categories = true }
 
       it "should show the new topic button only on categories" do
-        visit "/tag/#{tag1.name}"
+        visit tag1.url
         expect(page).to_not have_css(".new-topic-dropdown")
 
         visit "/c/#{cat.slug}"
@@ -269,7 +268,7 @@ RSpec.describe "Preset Topic Composer | preset topic creation" do
       before { SiteSetting.hide_unmatched_composer_presets = true }
 
       it "should hide unmatched composer presets" do
-        visit "/tag/#{tag1.name}"
+        visit tag1.url
         expect(page).to have_css(".new-topic-dropdown")
 
         preset_dropdown = PageObjects::Components::PresetTopicDropdown.new
